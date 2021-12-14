885986d7793d21d9c77701577f852594cbafebc5
Renan Bueno <renanbuenov@hotmail.com>
2021-12-13 21:29:01 -0300

    Project initiated

---
+++ src/test/java/renan/purchaseservice/PurchaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package renan.purchaseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
